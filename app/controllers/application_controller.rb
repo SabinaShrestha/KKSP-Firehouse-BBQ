30d7a44e075d9126b106e9807dd519c6584586bf
Sabina Shrestha <sabina.pari@yahoo.com>
2018-06-30 13:44:22 -0600

    nothing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 	before_action :authenticate_user!, if: proc { request.controller_class.parent == Api }
-<<<<<<< HEAD
-
-	protected
-	def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-=======
->>>>>>> add item form
 end
