563f37e916d02dab64b36f7a62520f05c8f31299
Kira Kaur <kdeep97@gmail.com>
2018-06-30 13:52:08 -0600

    add devise sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
   include DeviseTokenAuth::Concerns::SetUserByToken
 	before_action :authenticate_user!, if: proc { request.controller_class.parent == Api }
+
+	protected
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
