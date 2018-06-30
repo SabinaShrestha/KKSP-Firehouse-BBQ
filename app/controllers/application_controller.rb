862a6eba7a4ff759c5cc172cef47b9440a10319a
Kira Kaur <kdeep97@gmail.com>
2018-06-30 11:46:48 -0600

    move controllers into api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 	before_action :authenticate_user!, if: proc { request.controller_class.parent == Api }
+
+	protected
+	
+	def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
